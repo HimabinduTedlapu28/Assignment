0178b8f40647032acdd5a3fb1b37ed5881727f7c
HimabinduTedlapu28 <144455404+HimabinduTedlapu28@users.noreply.github.com>
2023-09-10 23:03:08 -0700

    Add files via upload

---
+++ CSharp_Assignments/assignment01/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace assignment01
+{
+    class hello
+    { 
+        public static void calc()
+        { 
+            int num;
+            Console.Write("\n\n");
+            Console.WriteLine("Enter the number :");
+
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 0) 
+                Console.WriteLine("{0} is a Positive Number. \n", num);
+            else
+                    Console.WriteLine("It is a Negative Number. \n", num);
+                    Console.ReadLine();
+
+        }
+
+    }
+}
