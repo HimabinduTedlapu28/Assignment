bd3f134dc07d7370cf0b1ec26970733e8377607e
HimabinduTedlapu28 <144455404+HimabinduTedlapu28@users.noreply.github.com>
2023-09-23 22:34:11 +0530

    Add files via upload

---
+++ CSharp_Assignments/CONCESSION/concession app/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace concession_app
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+
+            Concession.concessioncalculator calculator = new Concession.concessioncalculator
+            {
+                Name = name,
+                Age = age
+            };
+
+            calculator.CalculateConcession();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+    
+
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
